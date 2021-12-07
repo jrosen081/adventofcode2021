2229e6f1da5207c50345b4500ccb19a54e46164e
jrosen081 <jrosen081@gmail.com>
2021-12-07 10:22:28 -0500

    Day 6 and 7

--- day6.rkt
+++ day6.rkt
@@ -16,9 +16,10 @@
   (define repros (hash-ref hash (cons n steps) #f))
   (cond [(number? repros) repros]
         [(zero? steps) 1]
-        [else (let ((total (for/sum ([num (if (zero? n) (list 6 8) (list (sub1 n)))])
-                     (total-reproductions num (sub1 steps) hash))))
-        (hash-set! hash (cons n steps) total)
-        total)]))
+        [else
+         (let ((total (for/sum ([num (if (zero? n) (list 6 8) (list (sub1 n)))])
+                        (total-reproductions num (sub1 steps) hash))))
+           (hash-set! hash (cons n steps) total)
+           total)]))
 
 (define INPUT '(1 1 1 2 1 5 1 1 2 1 4 1 4 1 1 1 1 1 1 4 1 1 1 1 4 1 1 5 1 3 1 2 1 1 1 2 1 1 1 4 1 1 3 1 5 1 1 1 1 3 5 5 2 1 1 1 2 1 1 1 1 1 1 1 1 5 4 1 1 1 1 1 3 1 1 2 4 4 1 1 1 1 1 1 3 1 1 1 1 5 1 3 1 5 1 2 1 1 5 1 1 1 5 3 3 1 4 1 3 1 3 1 1 1 1 3 1 4 1 1 1 1 1 2 1 1 1 4 2 1 1 5 1 1 1 2 1 1 1 1 1 1 1 1 2 1 1 1 1 1 5 1 1 1 1 3 1 1 1 1 1 3 4 1 2 1 3 2 1 1 2 1 1 1 1 4 1 1 1 1 4 1 1 1 1 1 2 1 1 4 1 1 1 5 3 2 2 1 1 3 1 5 1 5 1 1 1 1 1 5 1 4 1 2 1 1 1 1 2 1 3 1 1 1 1 1 1 2 1 1 1 3 1 4 3 1 4 1 3 2 1 1 1 1 1 3 1 1 1 1 1 1 1 1 1 1 2 1 5 1 1 1 1 2 1 1 1 3 5 1 1 1 1 5 1 1 2 1 2 4 2 2 1 1 1 5 2 1 1 5 1 1 1 1 5 1 1 1 2 1))
